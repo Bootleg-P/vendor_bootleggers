61887b84030e5bae0f9a4fb1fbb1915faa03c47b
varunhardgamer <gamelovr695@gmail.com>
2019-11-15 15:01:47 +0000

    vendor: Kick a few prebuilt apps

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -40,35 +40,6 @@ include $(BUILD_PREBUILT)
 #
 # Prebuilt APKs
 #
-#Gallery app
-include $(CLEAR_VARS)
-LOCAL_MODULE := CameraRoll
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := Gallery Gallery2 SnapdragonGallery VanillaGallery
-include $(BUILD_PREBUILT)
-
-#FileManager App
-include $(CLEAR_VARS)
-LOCAL_MODULE := MiXplorerPrebuilt
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := CMFileManager Amaze AmazeFM crDroidFileManager
-include $(BUILD_PREBUILT)
-
-#ShishuWalls, my photo wallpaper app
-include $(CLEAR_VARS)
-LOCAL_MODULE := ShishuWalls
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
 # WallpaperPickerGoogle
 include $(CLEAR_VARS)
 LOCAL_MODULE := WallpaperPickerGoogle
@@ -89,27 +60,6 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_DEX_PREOPT := false
-include $(BUILD_PREBUILT)
-
-#Clock App
-include $(CLEAR_VARS)
-LOCAL_MODULE := GugelClock
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := DeskClock OmniClock DeskClockGoogle GDeskClock
-include $(BUILD_PREBUILT)
-
-#Launcher App
-include $(CLEAR_VARS)
-LOCAL_MODULE := Lawnchair
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo Quickstep
 include $(BUILD_PREBUILT)
 
 #Longshot for screenshots
